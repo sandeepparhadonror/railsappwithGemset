8ea1975dcf0bbf21a4b3799c96f95ed8e507f2de
sandeepparhadonror <sandeepparhadonror@gmail.com>
2014-02-24 22:41:54 +0530

    gemset

---
+++ how to create project with ruby 1.9.3 and rails 3.2.XX using gemset.rb
@@ -0,0 +1,22 @@
+how to create project with ruby 1.9.3 and rails 3.2.XX using gemset
+
+
+rvm gemset create rails_3.2_xx
+
+rvm gemset create rails_4.0.xx
+
+rvm gemset list 
+  rails_3.2.xx
+  rails_4.0.xx
+
+rvm gemset use rails_3.2.xx
+
+rvm use ruby-1.9.3-p484@rails_3.2.xx --create
+
+gem install rails --version 3.2.14
+
+than set the project with ruby 1.9.3 and rails 3.2.14
+
+heml  =>  http://stackoverflow.com/questions/18290080/change-rails-version-used-by-rvm
+
+
