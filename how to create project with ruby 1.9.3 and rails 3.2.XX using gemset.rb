94bf5056851e494803577688d4ee36d589d59ee6
sandeep parhad <sandeepparh@cybage.com>
2014-12-30 01:17:56 +0530

    rails gemset file update

--- how to create project with ruby 1.9.3 and rails 3.2.XX using gemset.rb
+++ how to create project with ruby 1.9.3 and rails 3.2.XX using gemset.rb
@@ -13,6 +13,8 @@ rvm gemset use rails_3.2.xx
 
 rvm use ruby-1.9.3-p484@rails_3.2.xx --create
 
+rvm gemset use rails_3.2.xx
+
 gem install rails --version 3.2.14
 
 rvm use 1.9.3@rails-3.2.3 --default
